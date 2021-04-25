1362526a00f7f82cc709cb7bd2a14a2b863b0bf0
Ehsan masoudi <azimmasoudi@gmail.com>
2021-04-26 01:40:22 +0430

    changes added

---
+++ main.py
@@ -0,0 +1,20 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+timmy = Turtle()
+
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+    new_segment.go
+
+
+
+
+screen.exitonclick()
