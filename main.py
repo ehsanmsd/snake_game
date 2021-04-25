b1392e1c42399a22af227d3078e7ac7fdd2490d7
Ehsan masoudi <azimmasoudi@gmail.com>
2021-04-26 03:09:00 +0430

    changes added

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-from turtle import Screen, Turtle
+import time
+from turtle import Turtle, Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-timmy = Turtle()
+screen.tracer(0)
 
+snake = Snake()
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.goto(position)
-    new_segment.go
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 
 
